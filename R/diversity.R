ddfa1892185969b1feb10176d16ff52ab667c43a
mguevara <miguel.guevara@upla.cl>
2015-09-14 11:02:33 -0300

    RStirling, Distance and data att
    Refactoring Rao-Stirling diversity
    Distance function
    Attached external data

--- R/diversity.R
+++ R/diversity.R
@@ -105,7 +105,7 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
     diversity <- merge(diversity,m_d, by=0, all=TRUE)
     rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
-  if(type == 'inverse-simpson' || type=='inv' || type == 'all') {
+  if(type == 'inverse-simpson' || type=='is' || type == 'all') {
     q <- 2
     p <- 1/(1-q)
     m_d <- as.data.frame((rowSums(propX ^ q, na.rm=TRUE)) ^ p)
@@ -126,24 +126,47 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
     diversity <- merge(diversity,m_d, by=0, all=TRUE)
     rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
-  if(!is.null(method)) {
-    if (method == 'jaccard') {
-      disX <- as.matrix(dist(t(X), method="Jaccard"), diag=1)
-      
-    }
-    else if (method == 'euclidean') {
-      disX <- as.matrix(dist(t(X), method="euclidean"), diag=1)
-    }
-    else if (method == "cosine") {
-      disX <- as.matrix(dist(t(X), method="cosine"), diag=1)
-    }
-  }
-  else {
-    disX <- as.matrix(dist(t(X), method="euclidean"), diag=1)
-  }
   if(type == 'rao-stirling' || type=='rs' || type == 'all') {
-      m_d <- as.data.frame(rowSums(propX^beta %*% disX^alpha * propX^beta))
-      colnames(m_d) <- c('rao-stirling')
+  	disX <- distances(X, agg_type = agg_type, method=method) #compute distances first	
+  	disX_mask <- disX
+  	disX_mask[ (!is.na(disX_mask))] <- 1
+  	disX_mask[lower.tri(disX_mask)] <- 0
+  	diag(disX_mask) <- 0
+  	disX_mask[is.na(disX_mask)] <- 0
+  	
+  	#print(propX)
+  	N <- ncol(propX)
+  	m_d <- data.frame(row.names =  rownames(propX))
+  	str(m_d)
+  	m_d[,'rao-stirling'] <- NA
+ 
+  	for(entity in row.names(propX)) #go into each entity
+  	  {
+  	  	entity_data <- propX[entity,]
+  	  	prop_i <- matrix(entity_data, ,nrow=N,ncol=N, byrow=TRUE)
+  	  	prop_j <- matrix(entity_data, ,nrow=N,ncol=N, byrow=FALSE)
+  	  	p_ij <- prop_i * prop_j
+				p_ij_mask <- p_ij
+  			#p_ij_mask[p_ij_mask != 0] <- 1
+  			p_ij_mask[upper.tri(p_ij_mask)] <- 1
+  		  diag(p_ij_mask) <- 0
+  		  p_ij_mask[lower.tri(p_ij_mask)] <- 0
+  	  	rs_entity <- ((disX^alpha)*disX_mask) * ((p_ij^beta)*p_ij_mask)  #deleting proportions or distances inexistent that might become 1, because of potentia of zero.	
+  		  m_d[entity, 'rao-stirling'] <- sum(rs_entity)
+  		  print(entity)
+  		  print(p_ij)
+  		  print(p_ij_mask)
+  	    print((p_ij^beta)*p_ij_mask)
+  	  }
+  	#print(rs_entity)
+  	#print((p_ij^beta)*p_ij_mask)
+  	print(propX)
+  	print(disX)
+  	print(disX_mask)
+  	
+  	#print((disX^alpha)*disX_mask)
+  	  #m_d <- as.data.frame(rowSums(propX^beta %*% disX^alpha * propX^beta))
+      
       diversity <- merge(diversity,m_d, by=0, all=TRUE)
       rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
@@ -155,6 +178,8 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
   #}
   return(diversity)
 }
+
+
 
 #' @title Get Data
 #' @description It takes data as dataframe (edges) or as matrix (table) to be exported in proper form to be used by the diversity function.
@@ -168,6 +193,7 @@ get_data <- function(data, agg_type)
 	if (is.data.frame(data)) {
 		if(!is.null(agg_type)) {
 			#diversity <- data.frame(row.names=levels(data[,2]))
+			data <- droplevels(data) #delete un used levels
 			X <- matrix(0, nrow=nlevels(data[,2]), ncol=nlevels(data[,1]), dimnames=list(levels(data[,2]),levels(data[,1])))
 			X[cbind(data[,2], data[,1])] <- data[,3]
 		}
@@ -191,6 +217,27 @@ get_data <- function(data, agg_type)
 	
 }
 
+
+#' @title Variety
+#' @description It computes the variety or simple diversity of a system. Number of types
+#' @param data Data to be processed as dataframe or as matrix. 
+#' @examples 
+#' vari <- varity(data=d)
+#' @return a dataframe with values of variety
+#' @export
+variety <- function(data, sort=TRUE)
+{
+	vari <- diversity(data, type='v')
+	if(sort != FALSE)
+	{
+		vari['category'] <- row.names(vari)
+		vari <- vari[order(vari$variety, decreasing = TRUE), ]
+		vari['category'] <- NULL	
+	}
+	
+	return(vari)
+}
+
 #' @title Ubiquity
 #' @description It computes the ubiquity or the rearnes of the categories
 #' @param data Data to be processed as dataframe or as matrix. 
@@ -198,7 +245,6 @@ get_data <- function(data, agg_type)
 #' @examples 
 #' ub <- ubiquity(data=d)
 #' @return a dataframe with values of frequency per category. Decreasing order
-#' @export
 ubiquity <- function(data)
 {
 	ubiq <- diversity(data, type='v', method='euclidean' , agg_type='col')
@@ -250,22 +296,48 @@ readEdges <- function(path,sepr,we=TRUE){
   return(X)
 }
 
-#' @title A procedure to read a data frame from a csv file
-#' @description It takes a file and creates a data frame for diversity analysis
-#' @param path A string representing the path to a csv file. Rows and columns are described by strings (Ex.: "asind" "0010" 216000).
-#' @param sepr Separator field used in the file to separate columns
-#' @param we It indicates if the list of edges includes weights or not. Default is TRUE
-#' @return A data frame with objects as rows and categories as cols
+#' @title A procedure to read data of a data file in formats csv, dta or spss
+#' @description It reads a file with data shaped as a matrix or as edges list. Several types of formats are allowed.
+#' @param path A string representing the path to data file. If it is shpaed as a matrix, first column must include proper names of the categories. If it is shaped as edges list, it must contain three columns, these are entity, category, value. 
+#' @param sep Separator field used in the file to separate columns, if it is a CSV file. Default value is comma.
+#' @param type It indicates the type of data to be read. This parameter facilitate the input of diverse type of data files, as spss or stata Posible options are the names of the mentioned softwares. Default value is csv.
+#' @return A data frame with three columns, even when the input file is shaped as a matrix.
 #' @examples 
-#' path <- "~/MyDiversity/data/sitc_cnt_62.csv"
-#' sepr <- ' '
-#' we <- TRUE
-#' data <- readCSV(path,sepr,we)
-readCSV <- function(path,sepr,we=TRUE){
-  data <- data.frame()
-  #if (!we) col = 2 else col = 3
-  col_classes <- c('factor', 'factor', NA)
-  data <- rbind(data, read.csv(path, sep=sepr, colClasses=col_classes))
+#' path <-  path_to_matrix_file <- system.file("extdata", "PantheonMatrix.csv", package = "diveR")
+#' sep <- ','
+#' data <- read.data(path)
+#' path <-  path_to_matrix_file <- system.file("extdata", "PantheonEdges.csv", package = "diveR")
+#' data <- read.data(path)
+read.data <- function(path, type='csv',sep=','){
+
+	if(type=='csv')
+	{
+		data_temp <- read.csv(path, sep=sep, nrow=1)
+		if(ncol(data_temp)>3)
+		{#matrix shape
+			data <- read.csv(path,sep=sep)
+			data <- melt(data, na.rm = TRUE)
+		}
+		else
+		{
+			col_classes <- c('factor', 'factor', NA)
+			data <- read.csv(path, sep=sep, colClasses=col_classes)		
+		}
+		
+	}
+		if(type=='spss')
+	{
+			data <- read.spss(path, use.value.labels = FALSE)
+	}
+	if(type=='stata')
+	{
+		data <- read.dta(path)
+	}
+	if(ncol(data)> 3)
+	{
+		data <- melt(data, na.rm=TRUE)
+	}
+	row.names(data) <- NULL
   return(data)
 }
 
@@ -290,40 +362,19 @@ writeCSV <- function(frame, path, sepr) {
 #' @examples 
 #' Xdis <- dist_mat(data)
 #' Xdis <- dist_mat(data, method="jaccard", agg_type='col')
-dist_mat <- function(data, method=NULL, agg_type=NULL){
-  if (is.data.frame(data)) {
-    if(is.null(agg_type)) {
-      X <- matrix(0, nrow=nlevels(data[,2]), ncol=nlevels(data[,1]), dimnames=list(levels(data[,2]),levels(data[,1])))
-      X[cbind(data[,2], data[,1])] <- data[,3]
-    }
-    else {
-      X <- matrix(0, nrow=nlevels(data[,1]), ncol=nlevels(data[,2]), dimnames=list(levels(data[,1]),levels(data[,2])))
-      X[cbind(data[,1], data[,2])] <- data[,3]
-    }
-  }
-  else {
-    if (!is.null(agg_type)) {
-      X <- t(data)
-    }
-    else {
-      X <- data
-    }  
-  }
-  if(!is.null(method)) {
+distances <- function(data, method='cosine', agg_type=NULL){
+    X <- get_data(data=data, agg_type=agg_type)
+  
     if (method == 'jaccard') {
-      disX <- as.matrix(dist(t(X), method="Jaccard"), diag=1)
-      
+      disX <- as.matrix(dist(t(X), method="Jaccard"), diag=1)  
     }
-    else if (method == 'euclidean') {
+		if (method == 'euclidean') {
       disX <- as.matrix(dist(t(X), method="euclidean"), diag=1)
     }
-    else if (method == "cosine") {
+    if (method == "cosine") {
       disX <- as.matrix(dist(t(X), method="cosine"), diag=1)
     }
-  }
-  else {
-    disX <- as.matrix(dist(t(X), method="cosine"), diag=1)
-  }
+
   return(disX)
 }
 
@@ -337,7 +388,7 @@ dist_mat <- function(data, method=NULL, agg_type=NULL){
 #' Xdis <- dist_mat(data, method="euclidean")
 #' disp <- disparity(Xdis)
 #' disp <- disparity(Xdis, method="avg", agg_type='col')
-disparity <- function(X, method="all", agg_type="row") {
+disparity <- function(X, method="all", agg_type="col") {
   if (agg_type == "col") {
     X <- t(X)
     disparity <- data.frame(row.names=rownames(X))
@@ -358,4 +409,38 @@ disparity <- function(X, method="all", agg_type="row") {
     rownames(disparity) <- disparity$Row.names; disparity$Row.names <- NULL
   }
   return(disparity)
+}
+
+#' @title Balance
+#' @description A procedure to compute several measures of the dimension balance of the diversity.
+#' @param data A matrix of data with row and column names. Or a dataframe with three columns entity, category and value
+#' @examples 
+#' balance(data)
+balance <- function(data )
+{
+	balance <- diversity(data, type='entropy') #first balance measure
+	measures <- c( 'gini','simpson', 'berger-parker', 'inverse-simpson', 'evenness' )
+	for(measure in measures)
+	{
+		m_b <- diversity(data, type=measure)
+		balance <- merge(balance,m_b, by=0, all=TRUE)
+		rownames(balance) <- balance$Row.names; balance$Row.names <- NULL
+	}
+	
+	return(balance)
+}
+
+
+
+#' @title A procedure to plot the matrix of data as a pheatmap
+#' @description It takes a matrix of data and plots a pheatmap of that matrix
+#' @param data A matrix of data to be ploted
+#' @param fontsize The size of the font used in the plot.
+#' @param fontsize_row The font size of the labels in the rows 
+#' @examples 
+#' pheatmap(data)
+plot.pheatmap <- function(data,title=NULL, fontsize=14, fontsize_row=7, color = c('blue','yellow'))
+{
+	pheatmap(data, cluster_rows=FALSE, cluster_cols=FALSE,main=paste(title),show_colnames=FALSE,legend=FALSE,fontsize = fontsize,fontsize_row=7, color=color)
+	
 }
