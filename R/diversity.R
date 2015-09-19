9cfafb762b2d04e7bd329464ad0460a53248dabd
mguevara <miguel.guevara@upla.cl>
2015-09-19 10:06:53 -0300

    Disparity function and documentation...

--- R/diversity.R
+++ R/diversity.R
@@ -1,20 +1,19 @@
 #' @title  Diversity measures
-#' @description It takes an object x, a category matrix and calculates a number of diversity measures   [!We need to consistent with the naming, e.g. either entitity or object!!]
-#' @param data A numeric matrix or data frame with objects as rows and categories as columns
-#' @param type A mnemonic string referencing the diversity measure. List of available measures: "variety", "entropy", "gini", "simpson", "true", "inverse-simpson", "herfindahl–hirschman", "renyi", "evenness", "rao-stirling". A list of short mnemonics for each measure: 'v', 'e', 'g', 's', 't', 'inv', 'hh', 're', ev', and 'rs'. The default for type is "all". More information for each measure in details and examples. 
-#' @param method "rao-stirling" uses a disparity function to measure the distance/similarity between objects. List of available disparity methods in the package: "cosine", "jaccard", "euclidean". The default for method is cosine.
+#' @description It receives an object with data especifying entities (systems), categories (species) and values of presence or abundance, and calculates a number of diversity measures.
+#' @param data A numeric matrix with entities as rows and categories as columns and cells as value of abundance. A dataframe with three columns: entities, categories, value of abundance.
+#' @param type A mnemonic string referencing the diversity measure. List of available measures: "variety", "entropy", "gini", "simpson", "true", "inverse-simpson", "herfindahl–hirschman","berger-parker", "renyi", "evenness", "rao","rao-stirling". A list of short mnemonics for each measure: 'v', 'e', 'g', 's', 'td', 'is', 'hh', 'bp,'re', ev', 'r',and 'rs'. The default for type is "all". More information for each measure in details and examples. 
+#' @param method "rao-stirling" and "rao" measures, use a disparity function to measure the distance between objects. For example: "cosine", "jaccard", "euclidean". The default for method is cosine. All distance measures availables in package proxy.
 #' @param agg_type aggregation type for diversity analysis. The analysis is conducted per row, but it can also be conducted by column via setting agg_type = "col". Default is NULL. 
 #' @param q parameter for true diversity index measure. This parameter is also used for the Rényi entropy. Default is 0.
 #' @param alpha parameter for Rao-Stirling diversity measure. As default we consider alpha=1.
 #' @param beta parameter for Rao-Stirling diversity measure. As default we consider beta=1.
-#' @details Available diversity measures are (written for an object x category matrix): 
-#' [!! the expression "object x caegory matrix" is hardly understandable. Moroever I think you could delete the parenthesis after are ( ...!!]
+#' @details Available diversity measures are:
 #' 
 #' Variables: N (category count), p_i (proportion of system comprises category i), d_ij (disparity between i and j).
 #' 
-#' variety: N, category counts per object [MacArthur 1965]
+#' variety: N, category counts per system [MacArthur 1965]
 #' 
-#' entropy: - sum(p_i log p_i), Shannon entropy per object [Shannon 1948]
+#' entropy: - sum(p_i log p_i), Shannon entropy per system [Shannon 1948]
 #' 
 #' gini: 1 - sum(p_i^2), Gini-Simpson index per object [Gini 1912].  It is also known as the Gibbs–Martin index or the Blau index in sociology, psychology and management studies.
 #' 
@@ -22,7 +21,7 @@
 #' 
 #' true: (sum(p_i^q))(1-q)^-1, true diversity index per object [Hill 1973]. This measure is q parameterized. Default for q is 0.  
 #' 
-#' berger-parker: it is equals to the maximum p_i value in the dataset, i.e. the proportional abundance of the most abundant type. 
+#' berger-parker: it is equals to the maximum p_i value in the system, i.e. the proportional abundance of the most abundant type. 
 #' 
 #' inverse-simpson: (sum(p_i^2))^-1, inverse simpson index per object. This measure is the true diversity at q = 2.
 #' 
@@ -49,15 +48,20 @@
 #' 
 #' Stirling, A. (2007). "A General Framework for Analysing Diversity in Science, Technology and Society". Journal of the Royal Society Interface 4: 707-719.
 #' @examples
-#' X <- readEdges(path="~/MyDiversity/data/toy.edges", sepr=' ', we=TRUE)
-#' diversity(X, type="gini")
-#' diversity(X, type="rao-stirling", method="cosine")
-#' diversity(X, type="all", method="jaccard")
+#' #reading csv data matrix
+#' path_to_file <- system.file("extdata", "PantheonMatrix.csv", package = "diveR")
+#' X <- read.data(path = path_to_file)
+#' diversity(data=X, type="gini")
+#' diversity(data=X, type="rao-stirling", method="cosine")
+#' diversity(data=X, type="all", method="jaccard")
 #' 
-#' data <- readCSV(path="~/MyDiversity/data/sitc_cnt_62.csv", sepr=' ', we=TRUE)
-#' diversity(data, type="gini")
-#' diversity(data, type="rao-stirling", method="cosine")
-#' diversity(data, type="all", method="jaccard")
+#' #reading csv dataframe
+#' path_to_file <- system.file("extdata", "PantheonEdges.csv", package = "diveR")
+#' X <- read.data(path = path_to_file)
+#' #true diversity
+#' diversity(data=X, type="td", q=1)
+#' #rao stirling with differente parameters
+#' diversity(data=X, type="rao-stirling", method="euclidean", alpha=0, beta=1)
 #' @export
 diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, alpha=1, beta=1){
   X <- get_data(data, agg_type)
@@ -93,7 +97,7 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
     diversity <- merge(diversity,m_d, by=0, all=TRUE)
     rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
-  if(type == 'true' || type=='t' || type == 'all') {
+  if(type == 'true' || type=='td' || type == 'all') {
     p <- 1/(1-q)
     m_d <- as.data.frame((rowSums(propX ^ q, na.rm=TRUE)) ^ p)
     colnames(m_d) <- c('true-diversity')
@@ -127,7 +131,7 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
     diversity <- merge(diversity,m_d, by=0, all=TRUE)
     rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
-  if(type == 'rao-stirling' || type=='rs' || type == 'all') {
+  if(type == 'rao-stirling' || type=='rs' || type == 'all' || type=='rao' || type=='r'){
   	disX <- distances(X, agg_type = agg_type, method=method) #compute distances first	
   	disX_mask <- disX
   	disX_mask[ (!is.na(disX_mask))] <- 1
@@ -138,22 +142,41 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
   	#print(propX)
   	N <- ncol(propX)
   	m_d <- data.frame(row.names =  rownames(propX))
-  	str(m_d)
-  	m_d[,'rao-stirling'] <- NA
- 
+  	#str(m_d)
+  	if(type == 'rao-stirling' || type=='rs' || type=='all')
+  	{
+  		m_d[,'rao-stirling'] <- NA; ms_label <- 'rao-stirling'	
+  	}
+  	if(type=='rao' || type=='r')
+  	{
+  		m_d[,'rao'] <- NA	 ;  ms_label <- 'rao'
+  	}
+  	if(type=='disparity' || type=='d')
+  	{
+  		m_d[,'disparity_sum'] <- NA	; ms_label <- 'disparity_sum'
+  	}
   	for(entity in row.names(propX)) #go into each entity
   	  {
   	  	entity_data <- propX[entity,]
-  	  	prop_i <- matrix(entity_data, ,nrow=N,ncol=N, byrow=TRUE)
-  	  	prop_j <- matrix(entity_data, ,nrow=N,ncol=N, byrow=FALSE)
+  	  	prop_i <- matrix(entity_data, nrow=N,ncol=N, byrow=TRUE)
+  	  	prop_j <- matrix(entity_data, nrow=N,ncol=N, byrow=FALSE)
+  	  	if(ms_label=='disparity_sum')
+  	  	{
+  	  		prop_i<- prop_i[prop_i>0] <- 1 #binarizing proportion
+  	  		prop_j<- prop_j[prop_j>0] <- 1 #binarizing proportion
+  	  		alpha <- 1; beta<-1
+  	  		entity_variety <- length(entity_data[entity_data>0])
+  	  	}
   	  	p_ij <- prop_i * prop_j
 				p_ij_mask <- p_ij
   			#p_ij_mask[p_ij_mask != 0] <- 1
   			p_ij_mask[upper.tri(p_ij_mask)] <- 1
   		  diag(p_ij_mask) <- 0
   		  p_ij_mask[lower.tri(p_ij_mask)] <- 0
-  	  	rs_entity <- ((disX^alpha)*disX_mask) * ((p_ij^beta)*p_ij_mask)  #deleting proportions or distances inexistent that might become 1, because of potentia zero. [!!This sentence is unclear. You probably meand potential zero, but then still it is not clear what you precisely mean!??]	
-  		  m_d[entity, 'rao-stirling'] <- sum(rs_entity)
+  	  	rs_entity <- ((disX^alpha)*disX_mask) * ((p_ij^beta)*p_ij_mask) #masks ensures for proportions to use only upper triangle matrix. For distances, to use only existant distances and upper triangle matrix.
+  		  m_d[entity, ms_label] <- sum(rs_entity)
+  		  if(type=='disparity' || type=='d')
+  		  { m_d[entity, 'disparity_mean'] <- sum(rs_entity)/entity_variety }
   		  print(entity)
   		  print(p_ij)
   		  print(p_ij_mask)
@@ -165,18 +188,10 @@ diversity <- function(data, type="all", method='euclidean', agg_type=NULL, q=0, 
   	print(disX)
   	print(disX_mask)
   	
-  	#print((disX^alpha)*disX_mask)
-  	  #m_d <- as.data.frame(rowSums(propX^beta %*% disX^alpha * propX^beta))
-      
       diversity <- merge(diversity,m_d, by=0, all=TRUE)
       rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
   }
-  #if(type == 'disparity' || type=='dis' || type == 'all') {
-    #  m_d <- as.data.frame(rowSums(disX))
-    #  colnames(m_d) <- c('disparity')
-    #  diversity <- merge(diversity,m_d, by=0, all=TRUE)
-    #  rownames(diversity) <- diversity$Row.names; diversity$Row.names <- NULL
-  #}
+  
   return(diversity)
 }
 
@@ -256,47 +271,6 @@ ubiquity <- function(data)
 	return(ubiq)
 }
 
-#' @title A procedure to read a matrix from a file
-#' @description It takes a file and creates a matrix for diversity analysis
-#' @param path A string representing the path to a file. Each row corresponds to a matrix row (Ex.: 1 2 0 1 1 0 represents a row a matrix with 6 cols)
-#' @param cols Number of columns of the matrix
-#' @param sepr Separator field used in the file to separate columns
-#' @return A matrix with objects as rows and categories as rows
-#' @examples 
-#' path <- "~/MyDiversity/data/toy.mat"
-#' cols <- 6
-#' sepr <- ' '
-#' X <- readMatrix(path,cols,sepr)
-#' @export
-readMatrix <- function(path,cols,sepr){
-  X <- matrix(scan(file=path, sep=sepr), ncol=cols, byrow=TRUE)
-  return(X)
-}
-
-#' @title A procedure to read a list of edges from a file
-#' @description It takes a file and creates a matrix for diversity analysis
-#' @param path A string representing the path to a file. Rows and columns are described by integers (Ex.: 1 2 1)
-#' @param sepr Separator field used in the file to separate columns
-#' @param we It indicates if the list of edges includes weights or not. Default is TRUE
-#' @return A matrix with objects as rows and categories as rows
-#' @examples 
-#' path <- "~/MyDiversity/data/toy.edges"
-#' sepr <- ' '
-#' we <- TRUE
-#' X <- readEdges(path,sepr,we)
-readEdges <- function(path,sepr,we=TRUE){
-  if (!we) col = 2 else col = 3
-  L <- matrix(scan(file=path, sep=sepr), ncol=col, byrow=TRUE)
-  rows <- max(L[,1])
-  cols <- max(L[,2])
-  X <- array(0, c(rows,cols))
-  if (col == 3) 
-    X[L[,1:2]] <- L[,3]
-  else
-    X[L[,1:2]] <- 1
-  return(X)
-}
-
 #' @title A procedure to read data of a data file in formats csv, dta or spss
 #' @description It reads a file with data shaped as a matrix or as edges list. Several types of formats are allowed.
 #' @param path A string representing the path to data file. If it is shaped as a matrix, the first column must include proper names of the categories. If it is shaped as edges list, it must contain three columns, which are entity, category, value. 
@@ -342,18 +316,6 @@ read.data <- function(path, type='csv',sep=','){
   return(data)
 }
 
-#' @title A procedure to write a diversity data frame to a csv file
-#' @description It takes a data frame and creates a csv file for diversity analysis
-#' @param frame A data frame with diversity measures
-#' @param path A string representing the path to a csv file
-#' @param sepr Separator field used in the file to separate columns
-#' @return A csv file with objects as rows and diversity measures as cols
-#' @examples 
-#' writeCSV(data,"~/MyDiversity/data/data.csv",' ')
-writeCSV <- function(frame, path, sepr) {
-  write.table(frame, path, sep=sepr, row.names=FALSE) 
-}
-
 #' @title A procedure to create a disparity matrix from a data frame or a matrix
 #' @description It takes a data frame or a matrix to create a disparity matrix
 #' @param data A data frame or matrix of objects x categories
@@ -379,36 +341,16 @@ distances <- function(data, method='cosine', agg_type=NULL){
   return(disX)
 }
 
-#' @title A procedure to analyze a disparity matrix
-#' @description It takes a disparity matrix to get average or sums of disparities
-#' @param data A disparity matrix to be analyzed
-#' @param method List of available disparity matrix analysis methods: "avg", "sum", "all". The default for method is "all".
+#' @title A procedure to compute the sum and average of disparities of systems
+#' @description It takes data of presence-abundance of categories in entities and computes the sum and average of disparities between categories PRESENT in the entity
+#' @param data A matrix or dataframe of entities, categories and values of presence 
+#' @param method a distance measure available in proxy package.
 #' @param agg_type aggregation type for disparity analysis. The analysis is conducted per row but it can also be performed by column setting agg_type = "col". Default is "row". 
-#' @return A data frame with disparity measures as columns for each object of data
+#' @return A data frame with disparity measures as columns for each entity of data. Sum of disparities and average of disparities are computed.
 #' @examples 
-#' Xdis <- dist_mat(data, method="euclidean")
-#' disp <- disparity(Xdis)
-#' disp <- disparity(Xdis, method="avg", agg_type='col')
-disparity <- function(X, method="all", agg_type="col") {
-  if (agg_type == "col") {
-    X <- t(X)
-    disparity <- data.frame(row.names=rownames(X))
-  }
-  else {
-    disparity <- data.frame(row.names=rownames(X))
-  }
-  if (method == 'sum' || method == 'v' || method == 'all') {
-    m_d <- as.data.frame(rowSums(X, na.rm=TRUE))
-    colnames(m_d) <- c('sum')
-    disparity <- merge(disparity, m_d, by=0, all=TRUE)
-    rownames(disparity) <- disparity$Row.names; disparity$Row.names <- NULL
-  }
-  if (method == 'avg' || method =='a' || method == 'all') {
-    m_d <- as.data.frame(rowMeans(X, na.rm=TRUE))
-    colnames(m_d) <- c('avg')
-    disparity <- merge(disparity, m_d, by=0, all=TRUE)
-    rownames(disparity) <- disparity$Row.names; disparity$Row.names <- NULL
-  }
+#' disp <- disparity(pantheon)
+disparity <- function(data, method='cosine', agg_type=NULL) {
+  disparity <- diversity(data=data, method=method, type='disparity')
   return(disparity)
 }
 
